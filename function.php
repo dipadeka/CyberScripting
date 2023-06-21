bc660af25a446a2501fbf09c85becb14703e6b25
bidyuttalukdar <bidyut3004@gmail.com>
2023-06-21 17:57:07 +0530

    php function wrote to take multiple files

---
+++ function.php
@@ -0,0 +1,10 @@
+<!-- $_FILES = [
+  'files' => [
+  	'name' => ['cars.jpg', 'bikes.png'],
+  	'type' => ['image/jpg', 'images/png'],
+  	'tmp_name' => ['/tmp/phpsw3jXq', '/tmp/php4kbqhq'],
+  	'error' => [0, 0],
+  	'size' => [268212, 95126]
+  ]
+];	 -->
+
