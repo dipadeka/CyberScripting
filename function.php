e1aae41a6e1d78a156eb6e3970e7e39023dce2a4
tikubidyuthere <bidyut3004@gmail.com>
2023-06-24 21:49:56 +0530

    Files are uploaded successfully

--- function.php
+++ function.php
@@ -4,11 +4,9 @@
 // Check if form was submitted
 if(isset($_POST['submit'])) {
  
-    // Configure upload directory and allowed file types
     $upload_dir = 'uploads'.DIRECTORY_SEPARATOR;
     $allowed_types = array('jpg', 'png', 'jpeg', 'gif');
      
-    // Define maxsize for files i.e 2MB
     $maxsize = 2 * 1024 * 1024;
  
     // Checks if user sent an empty form
@@ -22,18 +20,10 @@ if(isset($_POST['submit'])) {
             $file_size = $_FILES['files']['size'][$key];
             $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
  
-            // Set upload file path
             $filepath = $upload_dir.$file_name;
  
-            // Check file type is allowed or not
             if(in_array(strtolower($file_ext), $allowed_types)) {
- 
-                // Verify file size - 2MB max
-                // if ($file_size > $maxsize)        
-                //     echo "Error: File size is larger than the allowed limit.";
- 
-                // If file with name already exist then append time in
-                // front of name of the file to avoid overwriting of file
+
                 if(file_exists($filepath)) {
                     $filepath = $upload_dir.time().$file_name;
                      
@@ -55,16 +45,12 @@ if(isset($_POST['submit'])) {
                 }
             }
             else {
-                 
-                // If file extension not valid
                 echo "Error uploading {$file_name} ";
                 echo "({$file_ext} file type is not allowed)<br / >";
             }
         }
     }
     else {
-         
-        // If no files selected
         echo "No files selected.";
     }
 }
